1cd1860c529430b6dbba93974ae0f1e250de4418
VadMir <vmironov@mail.ru>
2017-12-04 17:31:45 +0400

    ok

---
+++ snmp_console2/SNMP_PDU.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <string>
+#include <memory>
+#include <vector>
+#include <map>
+
+#include "BER.h"
+
+typedef uint32_t PDUType;
+
+#define GETREQUEST		0xA0;
+#define GETNEXTREQUEST	0xA1;
+#define RESPONSE		0xA2;
+#define SETREQUEST		0xA3;
+#define GETBULKREQUEST	0xA5;
+#define INFORMREQUEST	0xA6;
+#define TRAP			0xA7;
+#define REPORT			0xA8;
+
+#define noError		0		//Нет ошибки
+#define	TooBig		1		//Слишком большой ответ для размещения в одном сообщении
+#define	NoSuchName	2		//Переменная не существует
+#define	BadValue	3		//Значение, которое должно быть сохранено, недопустимо
+#define	readOnly	4		//Значение не может быть изменено
+#define	genErr		5		//Другие ошибки
+
+class SNMP_PDU {
+	public:
+	SNMP_PDU();
+	~SNMP_PDU();
+
+	uint32_t version;
+	std::string community;
+
+	
+	PDUType pdutype;
+	uint32_t request_id;
+	uint32_t error_status;
+	uint32_t error_index;
+	
+	std::map<std::string, std::shared_ptr<BER>>variable_bindings;
+
+
+	static SNMP_PDU* SNMP_PDU::ParseBERtoSNMP_PDU(std::shared_ptr<BER>);
+};
--- snmp_console2/stdafx.h
+++ snmp_console2/stdafx.h
@@ -11,8 +11,10 @@
 #include <memory>
 #include <tchar.h>
 #include "BER.h"
+#include "SNMP_PDU.h"
 #include <iostream>
 using namespace std;
 // TODO: reference additional headers your program requires here
-short ber_sequence_parse(unsigned char *el, int length,vector<shared_ptr<BER>> value,short tab);
-std::string tab = "\t";
+short ber_sequence_parse(unsigned char *el, int length,vector<shared_ptr<BER>> *value,short tab);
+string oidtostr(vector<unsigned char>oid);
+string tab = "\t";
